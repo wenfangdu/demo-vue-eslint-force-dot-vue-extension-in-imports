e89f08b50913cb7e300b0a1fb2c565e1acc58c66
Artur Tagisow <atagisow@protonmail.com>
2020-07-12 10:58:08 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,22 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorldCopy/>
+    <HelloWorldCopy2/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
+import HelloWorldCopy from '@/components/HelloWorldWebpackAliasNoExtension'
+import HelloWorldCopy2 from '@/components/HelloWorldWebpackAliasWithExtension'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HelloWorldCopy,
+    HelloWorldCopy2,
   }
 }
 </script>
